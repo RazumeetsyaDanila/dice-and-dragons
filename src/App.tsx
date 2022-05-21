35a4c584f086b59137017362cd2a4da0c1ac6283
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2022-05-21 12:34:04 +0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,19 @@ import { useActions } from './hooks/useActions';
 import classes from './app.module.scss'
 import { useTypedSelector } from './hooks/useTypedSelector';
 import dragonImg from './img/dragons/Dragon.svg';
+import dragonKnightImg from './img/dragons/DragonKnight.svg';
+import DragonKnight1 from './img/dragons/DragonKnight1.svg';
+import DragonKnight2 from './img/dragons/DragonKnight2.svg';
 
 function App() {
   const { setDices } = useActions()
   const { dice, rollResult } = useTypedSelector(state => state.dices)
+  const allRollingsEnd = !dice[0].rolling && !dice[1].rolling && !dice[2].rolling && !dice[3].rolling && !dice[4].rolling && !dice[5].rolling
 
   return (
     <BrowserRouter>
       <div className={classes.container}>
-        {!dice[0].rolling && !dice[1].rolling && !dice[2].rolling && !dice[3].rolling && !dice[4].rolling && !dice[5].rolling ?
+        {allRollingsEnd ?
           <div className={classes.rollResult}>
             <p>Attack: {rollResult.attack ? <span className={classes.countSpan}>{rollResult.attack}</span> : 0}</p>
             <p>Life: {rollResult.life ? <span className={classes.countSpan}>{rollResult.life}</span> : 0}</p>
@@ -36,16 +40,34 @@ function App() {
           </div>
         }
         {/* дракон */}
-        <img src={dragonImg} alt="..." />
-        <div className={classes.dicesContainer}>
-          <div className={classes.diceContainer}>
-            {
-              dice.map(d => <Dice diceId={d._id} key={d._id} />)
-            }
+
+        {/* <img src={dragonImg} alt="..." /> */}
+        {/* <img src={dragonKnightImg} alt="..." /> */}
+
+
+        <div className={classes.podlozhka}>
+          <div className={classes.dragon_circle_knight}>
+            <img src={DragonKnight1} alt="..." />
+            <div className={classes.circle}></div>
+            <img src={DragonKnight2} alt="..." />
           </div>
-          <div className={classes.rollBtn} onClick={setDices}> ROLL ALL </div>
+
+          <div className={classes.kvadrat}>
+            <div className={classes.dicesContainer}>
+              <div className={classes.diceContainer}>
+                {
+                  dice.map(d => <Dice diceId={d._id} key={d._id} />)
+                }
+              </div>
+              <div className={classes.rollBtn} onClick={setDices}> ROLL ALL </div>
+            </div>
+          </div>
         </div>
+
       </div>
+
+
+
     </BrowserRouter>
   );
 }
